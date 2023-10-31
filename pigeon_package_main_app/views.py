c1832f35c17b7dcf308d58916410c522246cd839
Dmitry Chupakhin <chupahkin@gmail.com>
2023-11-01 00:10:44 +0300

    Update registration and main-new-project

--- pigeon_package_main_app/views.py
+++ pigeon_package_main_app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request=request, template_name='pigeon_package_main_app/main.html')
+    return render(request=request, template_name='pigeon_package_main_app/main-new-project.html')
